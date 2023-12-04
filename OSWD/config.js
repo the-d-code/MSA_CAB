31f59dc378e51c2731926e4cdd35edf173c773d5
Devanshi Akbari <devanshiakbari.mscict2022@vnsgu.ac.in>
2023-12-04 09:02:35 +0530

    OUTER

---
+++ OSWD/config.js
@@ -0,0 +1,12 @@
+const SecretKey = "N2nA1aY0yA1aN2nC0cH0hA1aB0bH1hA2aD3dI4iY5yA6a7";
+const LocalUrl = "mongodb://localhost:27017/exam";
+const Port = 8000;
+const Host = "localhost";
+
+
+module.exports = {
+  SecretKey,
+  LocalUrl,
+  Port,
+  Host,
+};
